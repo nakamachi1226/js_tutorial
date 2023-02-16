b5ba2e4c4654d66b1ec2929073012e0721f63b91
nakamachi1226 <harutoshi.ishikawa1@gmail.com>
2023-02-16 10:09:35 +0900

    計算履歴を追加

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var mytxt1 = document.getElementById("mytxt1");
 var mytxt2 = document.getElementById("mytxt2");
 var myselect = document.getElementById("myselect");
-var mycalc;
+var ul = document.getElementById("ul");
+var result;
+var calc;
 
 var myfunc = function(){
     if(myselect.value=="1"){
-        mycalc = Number(mytxt1.value) + Number(mytxt2.value);
+        result = Number(mytxt1.value) + Number(mytxt2.value);
+        calc = "+";
     }else{
-        mycalc = Number(mytxt1.value) - Number(mytxt2.value);
+        result = Number(mytxt1.value) - Number(mytxt2.value);
+        calc = "-";
     };
-    alert(mycalc);
+    const li = document.createElement("li");
+    li.innerText = mytxt1.value + calc + mytxt2.value + "=" + result;
+    ul.appendChild(li);
+    alert(result);
 }
-
