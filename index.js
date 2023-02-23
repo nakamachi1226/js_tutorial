4744728b24e5addf3682e0aa4d7c96caca92a34c
nakamachi1226 <harutoshi.ishikawa1@gmail.com>
2023-02-23 10:31:23 +0900

    クリア機能追加

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var mytxt1 = document.getElementById("mytxt1");
 var mytxt2 = document.getElementById("mytxt2");
 var myselect = document.getElementById("myselect");
 var ul = document.getElementById("ul");
+var len;
 var result;
 var calc;
 
-var myfunc = function(){
+var funcCalc = function(){
     if(myselect.value=="1"){
         result = Number(mytxt1.value) + Number(mytxt2.value);
         calc = "+";
@@ -18,3 +19,15 @@ var myfunc = function(){
     ul.appendChild(li);
     alert(result);
 }
+
+var funcClear = function(){
+
+    // 子要素の数を取得
+    len = ul.children.length;
+    // 子要素を削除
+    for(var i=0; i<len; i++){
+        ul.removeChild(ul.children[0]);
+    }
+
+}
+
